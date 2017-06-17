437f374ee71b1eb210574c61661e1cbb2d092ccd
Error 404 - Name not found <bcr25939@kyois.com>
2017-06-17 22:24:38 +0100

    Update example.py

--- example.py
+++ example.py
@@ -7,7 +7,7 @@ def success(passwd):
 	print(f"\n Hash smashed:   {passwd}")
 	print(" Time completed: {}".format(time.strftime("%H:%M:%S")))
 	quit()
-
+#program output
 def fail():
 	print(f"\n Unable to break {userHash}")
 	print(" Time completed: {}".format(time.strftime("%H:%M:%S")))
@@ -16,7 +16,7 @@ def fail():
 def main(argv):
 	charset = "abcdefghijklmnopqrstuvABCDEFGHIJKLMNOPQRSTUVWXYZ123456789"
 	passwd = ""
-	userHash = argv[0]
+	userHash = argv[0] #get hash from user argument
 
 	print(f" Attemping to break {userHash}...")
 	print(" Time initiated: {}\n".format(time.strftime("%H:%M:%S")))
@@ -25,11 +25,14 @@ def main(argv):
 			for char2 in charset:
 				for char3 in charset:
 					for char4 in charset:
-						for char5 in charset:
+						for char5 in charset: #all these loops will cycle through the charset resulting however,
+							                #the goal is to use recursion instead of iteration to vary
+								        #the number of loops and times the characters will iterate
 							passwd=char1+char2+char3+char4+char5
 							hash=hashlib.md5(passwd.encode("utf-8")).hexdigest()
 							t=time.strftime("%H:%M:%S")
-							sys.stdout.write(f"\r |{t}| [{userHash}] : {hash} : {passwd}")
+							sys.stdout.write(f"\r |{t}| [{userHash}] : {hash} : {passwd}") #trust me, this
+							                                                             #looks 1337 as fuck
 							sys.stdout.flush()
 							if hash == userHash:
 								success(passwd)
